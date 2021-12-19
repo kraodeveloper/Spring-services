6ccde84e28a9a99b23cf39ceb9c0f9c2e82baee8
kraodeveloper <kraodeveloper@gmail.com>
2021-12-19 11:15:07 +0800

    add local cache

--- service-provider/src/main/java/org/jackrao/learning/ApplicationRunner.java
+++ service-provider/src/main/java/org/jackrao/learning/ApplicationRunner.java
@@ -3,8 +3,10 @@ package org.jackrao.learning;
 import com.alibaba.dubbo.config.spring.context.annotation.DubboComponentScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 @DubboComponentScan(basePackages = "org.jackrao.learning.provider")
 public class ApplicationRunner {
     public static void main(String[] args) {
---
+++ service-provider/src/main/java/org/jackrao/learning/configuration/CacheConfiguration.java
@@ -0,0 +1,27 @@
+package org.jackrao.learning.configuration;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.caffeine.CaffeineCache;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class CacheConfiguration {
+    public static final long SYSTEM_DEFAULT_EXPIRES = 4 * 60 * 1000;
+    @Bean
+    public CacheManager configCacheManager() {
+        CaffeineCacheManager manager = new CaffeineCacheManager();
+        manager.setCaffeine(Caffeine.newBuilder().expireAfterWrite(SYSTEM_DEFAULT_EXPIRES, TimeUnit.MILLISECONDS));
+        return manager;
+    }
+
+    @Bean(name = "generalCache")
+    public Cache getSettlementTTLCache() {
+        return new CaffeineCache("settlement", Caffeine.newBuilder().expireAfterAccess(SYSTEM_DEFAULT_EXPIRES, TimeUnit.MILLISECONDS).build());
+    }
+}
